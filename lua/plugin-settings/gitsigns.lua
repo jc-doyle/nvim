d85ffc668056a96e3222a8716beeeb41f3836395
JC-Doyle <doyle.jonty@gmail.com>
2022-07-14 17:03:30 +0200

    update deprec. warnings

--- lua/plugin-settings/gitsigns.lua
+++ lua/plugin-settings/gitsigns.lua
@@ -35,16 +35,16 @@ require('gitsigns').setup {
 	linehl = false,
 	signcolumn = false,
 	keymaps = {},
-	watch_index = {interval = 1000, follow_files = true},
+	watch_gitdir = {interval = 1000, follow_files = true},
 	current_line_blame = false,
-	current_line_blame_delay = 1000,
-	current_line_blame_position = 'eol',
+	current_line_blame_opts = {
+		delay = 1000,
+		virt_text_pos = 'eol',
+	},
 	sign_priority = 6,
 	update_debounce = 100,
 	status_formatter = nil,
 
 	-- Use default
 	word_diff = false,
-	use_decoration_api = true,
-	use_internal_diff = true,
 }
