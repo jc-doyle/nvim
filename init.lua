cafbf6c8a7cd0a0df0e519ed06dda113f0ed8942
JC-Doyle <doyle.jonty@gmail.com>
2022-08-03 21:18:19 +0200

    add matchparen

--- init.lua
+++ init.lua
@@ -3,9 +3,9 @@ vim.g.maplocalleader = "\\"
 vim.g.vim_in_focus = true
 vim.g.vsnip_snippet_dir = vim.fn.stdpath('config')..'/snippets'
 vim.g.loaded_netrw = 0
+vim.g.loaded_matchparen = 1
 
 require "plugins"
--- require "bib"
 require "settings"
 require "mappings"
 require "statusline"
