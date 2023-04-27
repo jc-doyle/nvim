4a9de451b7720b497cfa8a997bc14ce0540c805d
JC-Doyle <doyle.jonty@gmail.com>
2023-04-27 20:43:09 +0200

    settings & lsp update

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,43 +1,44 @@
 local lsp = require 'lspconfig'
 
 local servers = {
-	lua = 'lua_ls',
-	go = 'gopls',
-	java = 'jdtls',
-	python = 'pyright',
-	r = 'r_language_server',
-	json = 'jsonls',
-	sh = 'bashls',
-	zsh = 'bashls',
-	latex = 'texlab',
+  lua = 'lua_ls',
+  go = 'gopls',
+  java = 'jdtls',
+  python = 'pyright',
+  r = 'r_language_server',
+  json = 'jsonls',
+  sh = 'bashls',
+  zsh = 'bashls',
+  latex = 'texlab',
   html = 'html',
   emmet = 'emmet_ls',
   javascript = 'tsserver',
   css = 'cssls',
-	cpp = 'ccls',
-	vue = 'vls',
-	svelte = 'svelte',
+  cpp = 'ccls',
+  vue = 'vls',
+  svelte = 'svelte',
   rust = 'rust_analyzer',
   proto = 'bufls',
+  tailwind = 'tailwindcss',
 }
 
 local on_attach = function(client, bufnr)
-	require 'mappings.lsp'.set(client, bufnr)
+  require 'mappings.lsp'.set(client, bufnr)
 end
 
 for language, name in pairs(servers) do
-	local setup_path = 'lsp.servers.' .. language
-	local config = require(setup_path)
+  local setup_path = 'lsp.servers.' .. language
+  local config = require(setup_path)
 
   --if config.capabilities then
   --  config.capabilities =  require('cmp_nvim_lsp').update_capabilities(config.capabilities)
   --else
   --  config.capabilities =  require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
- --end
+  --end
 
-	config.on_attach = on_attach
+  config.on_attach = on_attach
 
-	lsp[name].setup(config)
+  lsp[name].setup(config)
 end
 
 require 'lsp.ui'
